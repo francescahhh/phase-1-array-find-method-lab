48db9dfb0272f07a30934888542787c7a6a3b671
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-11 00:31:12 -0700

    commit tree 07251884da7e97e848f524a6d5e04ced3864a3a8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function hasWin(obj) {
+    if (obj.result === "W") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function superbowlWin(teamData) {
+    const win = teamData.find(hasWin);
+    if (win) {
+    return win.year;
+} else {
+    return undefined;
+}
+}
+
+console.log(superbowlWin(record));
+
+
 
